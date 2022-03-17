86add945736a6c095e4a372152556a87e1370cae
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2022-03-17 09:45:55 -0600

    Exercise 1

--- app.ts
+++ app.ts
@@ -1,11 +1,46 @@
 (() => {
 
-    const a:number = 10;
-    console.log( a );
+    // Tipos
+    const batman:string = 'Bruce';
+    const superman:string = 'Clark';
+  
+    const existe:boolean = false;
+  
+    // Tuplas
+    const parejaHeroes: [string, string] = [batman,superman];
+    const villano: [string, number, boolean] = ['Lex Lutor',5,true];
+  
+    // Arreglos
+    const aliados: string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+  
+    //Enumeraciones
+    enum Power {
+      acuman = 0,
+      batman = 1,
+      flash = 5,
+      superman =100 
+    }
+    const fuerzaFlash:Power = Power.flash;
+    const fuerzaSuperman:Power = Power.superman;
+    const fuerzaBatman:Power = Power.batman;
+    const fuerzaAcuaman:Power = Power.acuman;
+  
+    // Retorno de funciones
+    function activar_batiseñal():string {
+      return 'activada';
+    }
+  
+    function pedir_ayuda():void {
+      console.log('Auxilio!!!');
+    }
+  
+    // Aserciones de Tipo
+    const poder: any = '100';
+    const largoDelPoder:number = (poder as string).length;
 
-})();
-
-
-// function sayHello( msg: string ) {
-//     console.log( msg + ' ' + 'Eduardo' );
-// }
+    console.log( largoDelPoder );
+  
+  
+  })()
+  
+  
