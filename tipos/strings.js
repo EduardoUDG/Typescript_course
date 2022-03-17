d27380ec41cc66bed0c9346086286ac82cb43928
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2022-03-16 23:18:11 -0600

    String

---
+++ tipos/strings.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    var _a;
+    const batman = 'Batman';
+    const linternaVerde = "linterna Verde";
+    const volcanNegro = `volcan Negro`;
+    console.log(((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || 'No está presente');
+})();
---
+++ tipos/strings.ts
@@ -0,0 +1,11 @@
+(()=> {
+
+    const batman:string = 'Batman';
+    const linternaVerde:string = "linterna Verde";
+    const volcanNegro:string = `volcan Negro`;
+
+    console.log( batman[10]?.toUpperCase() || 'No está presente' );
+
+
+
+})()
