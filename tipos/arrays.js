3aa0b253c95e025089fa8ce2fe1cfbb4378e7842
EduardoUDG <eduardo.cdorado@alumnos.udg.mx>
2022-03-17 00:03:57 -0600

    Arrays

---
+++ tipos/arrays.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    // const numbers: (string | number | boolean)[] = [1, 2, 3, 4, 5, '6', 7, 8, 9, 10];
+    // numbers.push( true );
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const villans = ['Omega Rojo', 'Dormamu', 'Duende Verde'];
+    villans.forEach(v => console.log(v.toUpperCase()));
+    console.log(villans);
+})();
